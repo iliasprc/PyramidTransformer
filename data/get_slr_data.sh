95f9d9a79404d97cf6b31f24d77cde3c63339f01
iliasprc <iliaspapastrat@gmail.com>
2021-03-07 00:15:42 +0200

    update config files

---
+++ data/get_slr_data.sh
@@ -0,0 +1,23 @@
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.001
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.002
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.003
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.004
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.005
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.006
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.007
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.008
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.009
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.010
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.011
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.012
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.013
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.014
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.015
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.016
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.017
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.018
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.001
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.002
+wget https://data.chalearnlap.cvc.uab.cat/AuTSL/data/train/train_set_vfbha39.zip.003
+
+
